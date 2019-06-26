ec1601a6788e2931bcce83ef0d7ed54cbac1d592
Alex Nibley <alexnibley@gmail.com>
2019-06-26 03:51:43 -0600

    Typo

--- src/components/Operation.vue
+++ src/components/Operation.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
     <p>Operation info</p>
-    <p>Operation code: {{ operation_code }} ({{ operation_codes[operation_code] }})</p>
+    <p>Operation code: {{ operationCode }} ({{ operationCodes[operationCode] }})</p>
     <hr>
     
     <!-- OPCODE 0 transfer -->
-    <div v-if="operation_code === 0">
+    <div v-if="operationCode === 0">
       <p>Amount:<asset-amount :amount="operation.amount" /></p>
       <p>Fee: <asset-amount :amount="operation.fee" /></p>
       <br>
@@ -27,135 +27,135 @@
     </div>
 
     <!-- OPCODE 5 account_create -->
-    <div v-else-if="operation_code === 5">
+    <div v-else-if="operationCode === 5">
     </div>
 
     <!-- OPCODE 6 account_update -->
-    <div v-else-if="operation_code === 6">
+    <div v-else-if="operationCode === 6">
     </div>
 
     <!-- OPCODE 7 account_whitelist -->
-    <div v-else-if="operation_code === 7">
+    <div v-else-if="operationCode === 7">
     </div>
 
     <!-- OPCODE 8 account_upgrade -->
-    <div v-else-if="operation_code === 8">
+    <div v-else-if="operationCode === 8">
     </div>
 
     <!-- OPCODE 9 account_transfer -->
-    <div v-else-if="operation_code === 9">
+    <div v-else-if="operationCode === 9">
     </div>
 
     <!-- OPCODE 15 asset_reserve -->
-    <div v-else-if="operation_code === 15">
+    <div v-else-if="operationCode === 15">
     </div>
 
     <!-- OPCODE 20 witness_create -->
-    <div v-else-if="operation_code === 20">
+    <div v-else-if="operationCode === 20">
     </div>
 
     <!-- OPCODE 21 witness_update -->
-    <div v-else-if="operation_code === 21">
+    <div v-else-if="operationCode === 21">
     </div>
 
     <!-- OPCODE 22 proposal_create -->
-    <div v-else-if="operation_code === 22">
+    <div v-else-if="operationCode === 22">
     </div>
 
     <!-- OPCODE 23 proposal_update -->
-    <div v-else-if="operation_code === 23">
+    <div v-else-if="operationCode === 23">
     </div>
 
     <!-- OPCODE 24 proposal_delete -->
-    <div v-else-if="operation_code === 24">
+    <div v-else-if="operationCode === 24">
     </div>
 
     <!-- OPCODE 25 withdraw_permission_create -->
-    <div v-else-if="operation_code === 25">
+    <div v-else-if="operationCode === 25">
     </div>
 
     <!-- OPCODE 26 withdraw_permission_create -->
-    <div v-else-if="operation_code === 26">
+    <div v-else-if="operationCode === 26">
     </div>
 
     <!-- OPCODE 27 withdraw_permission_claim -->
-    <div v-else-if="operation_code === 27">
+    <div v-else-if="operationCode === 27">
     </div>
 
     <!-- OPCODE 28 withdraw_permission_delete -->
-    <div v-else-if="operation_code === 28">
+    <div v-else-if="operationCode === 28">
     </div>
 
     <!-- OPCODE 29 committee_member_create -->
-    <div v-else-if="operation_code === 29">
+    <div v-else-if="operationCode === 29">
     </div>
 
     <!-- OPCODE 30 committee_member_update -->
-    <div v-else-if="operation_code === 30">
+    <div v-else-if="operationCode === 30">
     </div>
 
     <!-- OPCODE 31 committee_member_update_global_parameters -->
-    <div v-else-if="operation_code === 31">
+    <div v-else-if="operationCode === 31">
     </div>
 
     <!-- OPCODE 32 vesting_balance_create -->
-    <div v-else-if="operation_code === 32">
+    <div v-else-if="operationCode === 32">
     </div>
 
     <!-- OPCODE 33 vesting_balance_withdraw -->
-    <div v-else-if="operation_code === 33">
+    <div v-else-if="operationCode === 33">
     </div>
 
     <!-- OPCODE 34 worker_create -->
-    <div v-else-if="operation_code === 34">
+    <div v-else-if="operationCode === 34">
     </div>
 
     <!-- OPCODE 35 custom -->
-    <div v-else-if="operation_code === 35">
+    <div v-else-if="operationCode === 35">
     </div>
 
     <!-- OPCODE 36 assert -->
-    <div v-else-if="operation_code === 36">
+    <div v-else-if="operationCode === 36">
     </div>
 
     <!-- OPCODE 37 balance_claim -->
-    <div v-else-if="operation_code === 37">
+    <div v-else-if="operationCode === 37">
     </div>
 
     <!-- OPCODE 38 override_transfer -->
-    <div v-else-if="operation_code === 38">
+    <div v-else-if="operationCode === 38">
     </div>
 
     <!-- OPCODE 39 transfer_to_blind -->
-    <div v-else-if="operation_code === 39">
+    <div v-else-if="operationCode === 39">
     </div>
 
     <!-- OPCODE 40 blind_transfer -->
-    <div v-else-if="operation_code === 40">
+    <div v-else-if="operationCode === 40">
     </div>
 
     <!-- OPCODE 41 transfer_from_blind -->
-    <div v-else-if="operation_code === 41">
+    <div v-else-if="operationCode === 41">
     </div>
 
     <!-- OPCODE 43 asset_claim_fees -->
-    <div v-else-if="operation_code === 43">
+    <div v-else-if="operationCode === 43">
     </div>
 
     <!-- OPCODE 47 asset_claim_pool -->
-    <div v-else-if="operation_code === 47">
+    <div v-else-if="operationCode === 47">
     </div>
 
     <!-- OPCODE 49 custom_authority_create -->
-    <div v-else-if="operation_code === 49">
+    <div v-else-if="operationCode === 49">
     </div>
 
     <!-- OPCODE 50 custom_authority_update -->
-    <div v-else-if="operation_code === 50">
+    <div v-else-if="operationCode === 50">
     </div>
 
     <!-- OPCODE 51 custom_authority_delete -->
-    <div v-else-if="operation_code === 51">
+    <div v-else-if="operationCode === 51">
     </div>
   </div>
 </template>
